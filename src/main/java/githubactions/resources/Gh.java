6711d19797b4bbf49bf48967ac4a7c96e181b0a3
kohei3110 <kk.31108424@gmail.com>
2021-03-02 11:32:02 +0900

    Initial commit

---
+++ src/main/java/githubactions/resources/Gh.java
@@ -0,0 +1,11 @@
+package githubactions.resources;
+
+/**
+ * サンプルクラス。
+ * @author kohei
+ */
+public class Gh {
+    public String echo() {
+        return "hey";
+    }
+}
---
+++ src/test/java/githubactions/resources/GhTest.java
@@ -0,0 +1,30 @@
+package githubactions.resources;
+
+import static org.hamcrest.CoreMatchers.is;
+import org.mockito.MockitoAnnotations;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.mockito.InjectMocks;
+
+/**
+ * サンプルクラスのテストクラス。
+ * @author kohei
+ */
+public class GhTest {
+    
+    @InjectMocks
+    Gh gh;
+    
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+    
+     @Test
+     public void echo() {
+         String expected = "hey";
+         String actual = gh.echo();
+         assertThat(actual, is(expected));
+     }
+}
